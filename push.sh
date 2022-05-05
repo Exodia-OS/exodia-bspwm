3ff110afdc9e5eb806581dfab8ea44fe2f4d53f2
Mahmoud Mohammed <62524855+mmsaeed509@users.noreply.github.com>
2022-05-05 14:41:36 +0200

    update push script

--- push.sh
+++ push.sh
@@ -20,28 +20,41 @@ echo -e "\e[0;35m############################# \e[0m"
 Branch=$(git branch --show-current) 
 
 # get new updates if it founded #
-echo "Updating Repo"
+echo ""
+echo "#################"
+echo "# Updating Repo #"
+echo "#################"
 git pull 
 
-echo "Adding new changes to the repo"
+
+echo ""
+echo "##################################"
+echo "# Adding new changes to the repo #"
+echo "##################################"
 git add --all .
 
 if [ "$1" == "-m" ];
 then
     # commit changes#
+    echo ""
     git commit -m "$2"
 else
     # read commit comment from user #
-    echo "Write your commit comment! :- "
+    echo ""
+    echo "##################################"
+    echo "# Write your commit comment! :-  #"
     read yourCommit
 
     # commit changes#
+    echo ""
     git commit -m "$yourCommit"
 fi
 
 # push to repo #
+echo ""
 git push -u origin $Branch
 
+echo ""
 echo -e "\e[0;35m########################### \e[0m"
 echo -e "\e[0;35m#      Git Push Done      # \e[0m"
 echo -e "\e[0;35m########################### \e[0m"
