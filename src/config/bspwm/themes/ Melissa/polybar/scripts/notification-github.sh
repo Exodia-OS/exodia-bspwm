a4a387dae71a128e6275163a920c91f79e1621a3
Mahmoud Mohamed (00xWolf - mmsaeed509) <mmsaeed509@gmail.com>
2023-06-12 06:47:15 +0300

    updated src code

---
+++ src/config/bspwm/themes/ Melissa/polybar/scripts/notification-github.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+USER="username"
+# You can get your Personal access tokens from here : https://github.com/settings/tokens #
+TOKEN="accessToken"
+
+notifications=$(echo "user = \"$USER:$TOKEN\"" | curl -sf -K- https://api.github.com/notifications | jq ".[].unread" | grep -c true)
+
+echo "%{T3} $notifications"
+
